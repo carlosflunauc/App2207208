2fa426cb87cb13cd3376590364581d469e011ed0
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-11-12 21:35:08 -0500

    migration v2
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

---
+++ MiprimeraApp.Persistencia/Migrations/20211113023249_Ver2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MiprimeraApp.Persistencia.Migrations
+{
+    public partial class Ver2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
