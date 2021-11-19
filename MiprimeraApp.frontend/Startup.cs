bcdaac2b4ddc9c65861a6efe750d80f745cd26da
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-11-18 20:34:44 -0500

    Index1 funcionando el GetAllPacientes
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

--- MiprimeraApp.frontend/Startup.cs
+++ MiprimeraApp.frontend/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MiprimeraApp.Persistencia;
 
 namespace MiprimeraApp.frontend
 {
@@ -24,6 +25,8 @@ namespace MiprimeraApp.frontend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<IRepositorioPaciente, RepositorioPaciente>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
